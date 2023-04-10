d99ee2f306f8f1a24d60eef9afdaf84082673fd0
卢勇霏 <luyongfei@s-MacBook-Pro.local>
2023-04-10 14:38:19 -0700

    first company migration created

---
+++ api/jobapp/companies/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.db.models import URLField
+from django.utils.timezone import now
+
+
+class Company(models.Model):
+    class CompanyStatus(models.TextChoices):
+        LAYOFFS = "Layoffs"
+        HIRING_FREEZE = "Hirng Freeze"
+        HIRING = "Hiring"
+
+    name = models.CharField(max_length=30, unique=True)
+    status = models.CharField(
+        choices=CompanyStatus.choices, max_length=50, default=CompanyStatus.HIRING
+    )
+    last_update = models.DateTimeField(default=now, editable=True)
+    application_link = URLField(blank=True)
+    notes = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
